6ed8405a7cbcc1ffa38fdf633c73a1eab7d39a1e
alitemel89 <alitemel89.ce@gmail.com>
2020-05-10 03:28:24 +0300

    add function randomChoreGenerator

--- script.js
+++ script.js
@@ -9,6 +9,29 @@ let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
 
 
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreGenerator = () => {
+  numClosedDoors = 3;
+  const choreDoor = Math.floor(Math.random()*numClosedDoors);
+  if (choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = beachDoorPath;
+  } else if (choreDoor === 1) {
+    openDoor2 = botDoorPath;
+    openDoor3 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  } else (choreDoor === 2) {
+    openDoor3 = botDoorPath;
+    openDoor1 = spaceDoorPath;
+    openDoor2 = beachDoorPath;
+  }
+
+}
+
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
   
@@ -23,4 +46,3 @@ doorImage3.onclick = () => {
   doorImage3.src = spaceDoorPath;
   
 }
-
